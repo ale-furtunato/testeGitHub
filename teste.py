6bb671ad67ff4fbc5beb5d38eff6c6891ebb1858
Alexandre Furtunato <alexandrefurtunato02@gmail.com>
2023-12-07 14:23:29 -0300

    adicionei a subtracao

--- teste.py
+++ teste.py
@@ -2,3 +2,4 @@ a = int(input ('Digite um numero: '))
 b = int (input ('Digite outro numero: '))
 
 print (f"resultado da soma entre {a} e {b} e: {a + b}")
+print (f"resultado da subtracao entre {a} e {b} e: {a - b}")
